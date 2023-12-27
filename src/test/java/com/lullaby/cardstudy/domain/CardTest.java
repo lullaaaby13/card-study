3392d2045d48a8ecc72b11ca80a3717f7e21cb95
DongSeongSeo <sds901234@naver.com>
2023-12-27 21:59:18 +0900

    feat: github workflows 추가

--- src/test/java/com/lullaby/cardstudy/domain/CardTest.java
+++ src/test/java/com/lullaby/cardstudy/domain/CardTest.java
@@ -3,6 +3,7 @@ package com.lullaby.cardstudy.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.lullaby.cardstudy.fixture.CardSetFixture.CARDSET;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
@@ -10,7 +11,7 @@ class CardTest {
     @DisplayName("카드를 생성 한다.")
     @Test
     void create() {
-        Card card = new Card(null, "Apple", "사과");
+        Card card = new Card(CARDSET, "Apple", "사과");
         assertEquals("Apple", card.getFront());
         assertEquals("사과", card.getBack());
     }
@@ -18,7 +19,7 @@ class CardTest {
     @DisplayName("카드의 앞면을 수정 한다.")
     @Test
     void updateFront() {
-        Card card = new Card(null, "Apple", "사과");
+        Card card = new Card(CARDSET, "Apple", "사과");
         card.setFront("Banana");
         assertEquals("Banana", card.getFront());
     }
@@ -26,7 +27,7 @@ class CardTest {
     @DisplayName("카드의 뒷면을 수정 한다.")
     @Test
     void updateBack() {
-        Card card = new Card(null, "Apple", "사과");
+        Card card = new Card(CARDSET, "Apple", "사과");
         card.setBack("바나나");
         assertEquals("바나나", card.getBack());
     }
@@ -34,7 +35,7 @@ class CardTest {
     @DisplayName("카드의 암기 레벨을 증가 한다.")
     @Test
     void increaseMemorizationLevel() {
-        Card card = new Card(null, "Apple", "사과");
+        Card card = new Card(CARDSET, "Apple", "사과");
         card.increaseMemorizationLevel();
         assertEquals(MemorizationLevel.SomewhatDifficult, card.getMemorizationLevel());
     }
@@ -42,7 +43,7 @@ class CardTest {
     @DisplayName("카드의 암기 레벨을 감소 한다.")
     @Test
     void decreaseMemorizationLevel() {
-        Card card = new Card(null, "Apple", "사과");
+        Card card = new Card(CARDSET, "Apple", "사과");
         card.increaseMemorizationLevel();
         card.increaseMemorizationLevel();
         card.decreaseMemorizationLevel();
---
+++ src/test/java/com/lullaby/cardstudy/fixture/CardSetFixture.java
@@ -0,0 +1,7 @@
+package com.lullaby.cardstudy.fixture;
+
+import com.lullaby.cardstudy.domain.CardSet;
+
+public class CardSetFixture {
+    public static final CardSet CARDSET = new CardSet("TOEIC", "영단기");
+}
